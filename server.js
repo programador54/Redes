dad0b8e2c6c1c573244b1fab302b5a24d67adf45
Glitch (hello-express) <none>
2019-09-14 22:44:36 +0000

    🌆🎐 Checkpoint ./server.js:6736223/2142 ./config.json :6736223/168 ./watch.json:6736223/721 ./package.json:6736223/1286

---
+++ server.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client()
+client.prefix = config.prefix;
+
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)){
+        return message.reply("Olá meu prefixo é `-`")}
+    if(!message.content.startsWith(config.prefix)) return;
+
+let args = message.content.split(" ").slice(1);
+let command = message.content.split(" ")[0];
+command = command.slice(config.prefix.length);
+  try {
+      let commandFile = require(`./commands/${command}.js`);
+      delete require.cache[require.resolve(`./commands/${command}.js`)];
+      return commandFile.run(client, message, args);
+  } catch (err) {
+        console.error("Erro:" + err)
+  }
+})
+
+client.on("ready", () => {
+    console.log(`Bot foi iniciado com, ${client.users.size} usuários, ${client.guilds.size} servidores, ${client.channels.size} canais.`)
+
+    let messages = [`Assistindo ${client.users.size} pessoas`,
+                    `Estou em ${client.guilds.size} servidores`]
+
+    setInterval(() => {
+        let randomMessages = Math.floor(Math.random() * (messages.length - 1) + 1)
+        client.user.setActivity(messages[randomMessages])
+    }, 10000)
+
+    //0 = Jogando
+    //1 = Transmitindo
+    //2 = Ouvindo
+    //3 = Assistindo
+})
+
+client.login(process.env.TOKEN)
