eab5c214e306bc9258fb2e0f7736ac83f0000237
Glitch (hello-express) <none>
2019-09-15 02:14:13 +0000

    🌓🍵 Checkpoint ./commands/userinfo.js:6736223/2549 ./watch.json:6736223/5 ./commands/serverinfo.js:6736223/1125

---
+++ commands/serverinfo.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  const verlvl = {
+    0: "None",
+    1: "Low",
+    2: "Medium",
+    3: "(╯°□°）╯︵ ┻━┻",
+    4: "(ノಠ益ಠ)ノ彡┻━┻"
+  }
+
+    let inline = true
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .setThumbnail(sicon)
+    .setAuthor(message.guild.name)
+    .addField("Name", message.guild.name, inline)
+    .addField("💻 ID", message.guild.id, inline)
+    .addField(, message.guild.owner, inline)
+    .addField("Region", message.guild.region, inline)
+    .addField("Verification Level", verlvl[message.guild.verificationLevel],inline)
+    .addField("Members", `<:user:424958082691629057> ${message.guild.memberCount}`, inline)
+    .addField("Roles", message.guild.roles.size, inline)
+    .addField("Channels", message.guild.channels.size, inline)
+    .addField("You Joined", message.member.joinedAt)
+    .setFooter(`Created ${message.guild.createdAt}`);
+
+    message.channel.send(serverembed);
+
+    message.delete();
+}
+
+module.exports.help = {
+  name:"serverinfo"
+}
