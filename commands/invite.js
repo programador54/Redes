bc458b7e3a33a63031a509cbc25c084939700175
Glitch (hello-express) <none>
2019-09-15 21:01:11 +0000

    💸✊ Checkpoint ./commands/invite.js:6736223/128 ./watch.json:6736223/2

--- commands/invite.js
+++ commands/invite.js
@@ -5,6 +5,7 @@ module.exports.run = async (bot, message, args) => {
   let invite = new Discord.RichEmbed()
 
           .addField("Adicione a Dudinha no seu servidor! <a:amor:616722891198955530>", "Sou a Dudinha uma bot focada em administração e moderação.", true)
+          .setThumbnail("https://cdn.discordapp.com/avatars/622555910056706089/b866d29cc289bb49cefea1af7c5de7a9.png?size=2048")
           .addField(' <a:seta:617051958800547877> Me adicione em seu servidor:', '[clique aqui](https://discordapp.com/oauth2/authorize?client_id=622555910056706089&scope=bot&permissions=2146958847)', true)
 
 
