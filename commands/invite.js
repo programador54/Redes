8696ea23011363fcf4125884b445c614d665a5f3
Glitch (hello-express) <none>
2019-09-15 15:27:02 +0000

    👩‍👩‍👦🚦 Checkpoint ./commands/invite.js:6736223/599

--- commands/invite.js
+++ commands/invite.js
@@ -1,9 +1,9 @@
 const Discord = module.require('discord.js');
-
+var getJSON = require('get-json');
 
 module.exports.run = async (bot, message, args) => {
 
-  let invite = new Discord.RichEmbed()
+  var invite = new Discord.RichEmbed()
 
           .addField("__**" + "INVITE LINK: " + "**__", "https://discord.gg/TyM8AkG", true)
           .addField("__**" + "Bot Invite Link: " + "**__", "https://discordapp.com/oauth2/authorize?client_id=353154808078794752&scope=bot&permissions=2146958591", false)
