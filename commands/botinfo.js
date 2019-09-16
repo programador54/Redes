f269ef8a80fa9592f23afb70e6d8290deb06c90a
Glitch (hello-express) <none>
2019-09-16 21:41:24 +0000

    🍂🐔 Checkpoint ./commands/botinfo.js:6736223/50 ./watch.json:6736223/3

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
 const eEmbed = new Discord.RichEmbed()
-	.setColor('#0099ff')
+	.setColor('#ff80ed')
 	.setTitle('Dudinha 🌷')
-	.setURL('https://discord.js.org/')
 	.setDescription(' <a:amor:616722891198955530> Olá, sou a Dudinha , uma bot focada em administração e  moderação.')
   .setThumbnail('https://cdn.discordapp.com/attachments/623265409151402017/623265617037754398/a_741d9222075e0a1644468e04ed8b695d.gif')
 	.addField(' <a:hypezin:619885268987543564> Me adicione em seu servidor:', '[clique aqui](https://discordapp.com/oauth2/authorize?client_id=565601897365635114&scope=bot&permissions=2146958847)', true)
