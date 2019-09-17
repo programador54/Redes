f9a282627ec268cd5bb8c7296819993f99bb6c15
Glitch (hello-express) <none>
2019-09-17 20:29:35 +0000

    🚇🏬 Checkpoint ./commands/botinfo.js:6736223/180 ./commands/falar.js :6736223/795 ./watch.json:6736223/2 ./commands/infobot.js :6736223/596 ./commands/infobot.js:6736223/209

--- commands/infobot.js
+++ commands/infobot.js
@@ -7,6 +7,9 @@ const eEmbed = new Discord.RichEmbed()
   .setThumbnail('https://cdn.discordapp.com/attachments/623265409151402017/623265617037754398/a_741d9222075e0a1644468e04ed8b695d.gif')
 	.addField('👑 Meu criador', "! ♔ corιɴɢα' ๖̷̶̷ ̷F.G.C 🔥#0272", true)
 	.addField('<:node:623606982699647006> Versão do Bot', 'V 1.0.1', true)
+  .addField('Total Users', `${client.users.size}`, true)
+            .addField('Total Channels:', `${client.channels.size}`, true)
+            .addField('Total Servers', Math.ceil(client.guilds.size), true)
 	.setTimestamp()
 	.setFooter('Estou em desenvolvimento.', 'https://i.imgur.com/wSTFkRM.png');
 
