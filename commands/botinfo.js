9e45a44ffb9216c10153dcd46e83ffbd01677b75
Glitch (hello-express) <none>
2019-09-17 20:06:34 +0000

    ⏱🚁 Checkpoint ./commands/botinfo.js:6736223/447 ./watch.json:6736223/3

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,12 +3,11 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
 const eEmbed = new Discord.RichEmbed()
 	.setColor('#ff80ed')
-	.setTitle('Dudinha 🌷')
-	.setDescription(' <a:amor:616722891198955530> Olá, sou a Dudinha , uma bot focada em administração e  moderação.')
+	.setTitle('Sobre a Dudinha 🌷')
   .setThumbnail('https://cdn.discordapp.com/attachments/623265409151402017/623265617037754398/a_741d9222075e0a1644468e04ed8b695d.gif')
 	.addField('👑 Meu criador', "! ♔ corιɴɢα' ๖̷̶̷ ̷F.G.C 🔥#0272", true)
-	.addField(' Minha Linguagem', 'Ler mensagens, Escrever mensagens e Gerenciar mensagens', true)
-	.addField(' <a:fogo:616722909477732373> Precisa de ajuda?', 'Use ``d!``ajuda para ver meus comandos.' , true) 
+	.addField('<:node:623606982699647006> Versão do Bot', 'V 1.0.1', true)
+	.addField('🌎 Servidores', '${client.guilds.size}' , true) 
 	.setTimestamp()
 	.setFooter('Estou em desenvolvimento.', 'https://i.imgur.com/wSTFkRM.png');
 
