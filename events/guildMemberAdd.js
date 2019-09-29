449c267ef62fa7cc14bd20cd527d4eb75fa02d4c
Glitch (hello-express) <none>
2019-09-29 20:41:44 +0000

    🎽🍼 Checkpoint ./events/guildMemberAdd.js:6736223/1411 ./watch.json:6736223/4

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,28 @@
+const moment = require('moment')
+
+/**
+ * O evento guildMemberAdd é emitido após um membro entrar (ser adicionado em uma guild).
+ */
+
+module.exports = async (client, member) => {
+  // Verificações anti-selfbot de divulgação já que estamos tendo problemas com isso.
+  const daysSinceCreation = moment().diff(moment(member.user.createdAt), 'days')
+  const isDefaultAvatar = member.user.displayAvatarURL.startsWith('https://discordapp.com/')
+  const domaincount = member.user.username.match(/\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/)
+  if (domaincount > 0 && (isDefaultAvatar || daysSinceCreation < 3)) return (() => { member.send('Olá! você foi kickado automaticamente por suspeita de divulgação em nosso servidor. Contas com menos de 3 dias no discord não podem ter domínios (exemplo twitter.com)').catch(); member.kick('Autokick: Selfbots não são bem vindos').catch() })()
+
+  // Mensagem automatica de "bem-vindo"
+  const message = {
+    'content': '',
+    'embed': {
+      'color': 15407128,
+      'description': `🐧 Olá ${member.user.username}, seja bem vindo(a) ao Linux ON!`,
+      'author': {
+        'name': `${member.user.username}#${member.user.discriminator}`,
+        'icon_url': member.user.displayAvatarURL
+      }
+    }
+  }
+
+  member.guild.channels.get(process.env.GREETCHANNEL).send(message).catch()
+}
