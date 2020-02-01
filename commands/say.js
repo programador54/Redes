0ac9a8c903e012ec68c7aa3e8033b04cb70c6dfb
Glitch (hello-express) <none>
2020-02-01 12:11:38 +0000

    🎗💾 Checkpoint ./commands/say.js:6736223/426

--- commands/say.js
+++ commands/say.js
@@ -1,11 +1,23 @@
 const Discord = require("discord.js");
+const talkedRecently = new Set();
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, msg, args) => {
+  if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    } else {
 
-      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+
+      if(!msg.member.hasPermission("ADMINISTRATOR")) return;
       const sayMessage = args.join(" ");
-      message.delete().catch();
-      message.channel.send(sayMessage);
+      msg.delete().catch();
+      msg.channel.send(sayMessage);
+      
+      talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+    }
 
 }
 
