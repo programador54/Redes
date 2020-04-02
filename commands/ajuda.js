13bbc16ed6b4f3135a3b6edce2ebf0e250ec718f
Glitch (hello-express) <none>
2020-04-02 19:05:56 +0000

    🌲🎆 Checkpoint ./commands/ajuda.js:6736223/498

---
+++ commands/ajuda.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  
+  let embed = new Discord.RichEmbed()
+  .setDescription('`l.say` - Faz o bot dizer no canal do texto\n`l.clear` - Apaga mensagens no canal de texto.\n`l.avatar` - Mostra o avatar do membro mrmviom')
+}
