1dcbe34189012159ed64aa882242b609cf18f48e
Glitch (citrine-butterkase) <none>
2020-04-02 19:52:32 +0000

    🏌🍥 Checkpoint ./commands/clear.js :6736223/789 ./commands/say.js:6736223/369

--- commands/say.js
+++ commands/say.js
@@ -1,23 +1,11 @@
 const Discord = require("discord.js");
-const talkedRecently = new Set();
 
-module.exports.run = async (bot, msg, args) => {
-  if (talkedRecently.has(msg.author.id)) {
-            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
-    } else {
+module.exports.run = async (bot, message, args) => {
 
-
-      if(!msg.member.hasPermission("ADMINISTRATOR")) return;
+      if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send (`Você nao`);
       const sayMessage = args.join(" ");
-      msg.delete().catch();
-      msg.channel.send(sayMessage);
-      
-      talkedRecently.add(msg.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(msg.author.id);
-        }, 60000);
-    }
+      message.delete().catch();
+      message.channel.send(sayMessage);
 
 }
 
