98d6d9223e674d0a3015f6fb6d520cccc34fe81e
Glitch (citrine-butterkase) <none>
2020-04-02 20:03:34 +0000

    🚿😂 Checkpoint ./commands/say.js:6736223/44 ./commands/addmoney.js:6736223/733 ./commands/profile.js:6736223/1241 ./package.json:6736223/3303 ./watch.js:6736223/3

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-      if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send (`Você nao`);
+      if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send (`Você não têm permissão para usar este comando!`);
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
