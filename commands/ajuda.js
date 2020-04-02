62f58a2dc607169093827b7d0c547eccfd2301d8
Glitch (citrine-butterkase) <none>
2020-04-02 19:34:46 +0000

    Rewound to commit d805dc5e47863e73f12895dbb358788cc4e770b9 + 0 edits

--- commands/ajuda.js
+++ commands/ajuda.js
@@ -3,6 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   
   let embed = new Discord.RichEmbed()
-  .setField("Meu"'`l.say` - Faz o bot dizer no canal do texto\n`l.clear` - Apaga mensagens no canal de texto.\n`l.avatar` - Mostra o avatar do membro mencionado.')
+  .setAuthor('Solicitado por:' + message.author.username, message.author.avatarURL)
+  .setField("\nMeus comandos são:", '`l.say` - Faz o bot dizer por você no canal do texto\n`l.clear` - Apaga mensagens no canal de texto.\n`l.avatar` - Mostra o avatar do membro mencionado.')
+  .setFooter(`${message.guild.name}`, message.guild.iconURL)
+  .setTimestamp()
   message.channel.send(embed)
 }
