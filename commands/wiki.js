1f0cc974b4bcfdacfb328586ca99181c52bd44d3
Glitch (citrine-butterkase) <none>
2020-04-30 22:45:18 +0000

    🍞🚔 Checkpoint ./commands/wiki.js:6736223/582

--- commands/wiki.js
+++ commands/wiki.js
@@ -3,14 +3,20 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   //!say Hi!
   //Hi
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`<a:atento:674202798803517447> | Desculpe, <@${message.author.id}>. Para utilizar este comando é necessário que você tenha a permissão de \n\`\`GERENCIAR MENSAGENS\`\`!`).then(msg => msg.delete(15000));
+
   let botmessage = args.join(" ");
   message.delete().catch();
   let emb = new Discord.RichEmbed()
   .setAuthor(`➕ | Novo wiki, feito por: ${message.author.tag}`)
   .setDescription(`${botmessage}`) 
+  .setThumbnail(message.author.avatarURL) 
   .setColor("#9370DB") 
-  message.guild.channels.get('705542186032300032').send(emb);
+  .setFooter(`(✅) Gostou | (❌) Não gostou`)
+  message.guild.channels.get('705542186032300032').send(emb).then(async function (message) {
+		await message.react("✅")
+		await message.react("❌")
+  }) 
+                        
 }
 
 module.exports.help = {
