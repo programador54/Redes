4c506879a18a3892c8c82a221540d2f4ade28427
Glitch (citrine-butterkase) <none>
2020-05-04 02:29:54 +0000

    ⛈⛑ Checkpoint ./commands/addbot.js:6736223/716

--- commands/addbot.js
+++ commands/addbot.js
@@ -5,27 +5,31 @@ module.exports = {
 
         if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Você não pode usar esse comando.")
         message.delete().catch()
-
+       
+        let dUser = message.guild.members.get("555450363789180958")
         let splitarg = args.join(" ").split(" / ")
         let titulo = splitarg[0]
         let anuncio = splitarg[1]
 
         if(!titulo){
-            message.reply("** <a:setinha:630093687036313641> Use o formato:\n ``l.addbot <ID do bot> / <anuncio>``**")
+            message.reply("** <a:setinha:630093687036313641> Use o formato:\n ``l.addbot <ID do bot> / <descrição>``**")
             return
         }
 
         if(!anuncio){
-            message.reply("** <a:setinha:630093687036313641> Use o formato:\n ``x-anunciar <ID do bot> / <anuncio>**")
+            message.reply("** <a:setinha:630093687036313641> Use o formato:\n ``l.addbot <ID do bot> / <descrição>**")
             return
         }
 
         let anuncioembed = new Discord.RichEmbed()
         .setColor("#FF9900")
-        .addField(`${titulo}`, `**${anuncio}**`)
+        .setTitle("🔔 | Novo pedido!") 
+        .setThumbnail(message.author.avatarURL) 
+        .addField(`ID do Bot: ${titulo}`, `Descrição: **${anuncio}**`)
         .setFooter(`Anuncio feito por ${message.author.tag}`)
         .setTimestamp();
 
-        message.channel.send(anuncioembed)
+        dUser.send(anuncioembed)
+      message.channel.send('✅ | **Bot enviado para ser adicionado, aguarde a análise do seu bot para saber se ele é seguro**!').then(msg => msg.delete(1500))
     }
 }
