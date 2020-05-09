5b5b62ac68f565635764a082356b803ad8555720
Glitch (citrine-butterkase) <none>
2020-05-09 12:09:20 +0000

    💎💂 Checkpoint ./commands/addbot.js:6736223/361

--- commands/addbot.js
+++ commands/addbot.js
@@ -5,7 +5,7 @@ module.exports = {
 
       
         message.delete().catch()
-       
+        let user2 = message.guild.member.get("681145371527086098")
         let dUser = message.guild.members.get("555450363789180958")
         let splitarg = args.join(" ").split(" / ")
         let titulo = splitarg[0]
@@ -27,9 +27,11 @@ module.exports = {
         .setThumbnail(message.author.avatarURL) 
         .addField('💻 ID do Bot:', `\`\`${titulo}\`\``) 
         .addField('📋 Descrição:', `**${anuncio}**`)
+        .addField(':link: Link do Bot:', `**[Clique aqui para adicionar o bot](https://discord.com/api/oauth2/authorize?client_id=${titulo}&permissions=1647692865&scope=bot)**`)
         .setFooter(`Enviado por: ${message.author.tag}`, message.author.avatarURL) 
         .setTimestamp();
-
+        
+        user2.send(anuncioembed) 
         dUser.send(anuncioembed)
       message.channel.send('✅ | **Bot enviado para ser adicionado, aguarde a análise do seu bot para saber se ele é seguro**!').then(msg => msg.delete(1500))
     }
