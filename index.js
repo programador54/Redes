6c4f5caad8c15c2a8d2613c3b22b8d23b435a5fd
programador54 <52678460+programador54@users.noreply.github.com>
2020-06-29 06:02:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+const Discord = require('discord.js');
+const fs = require("fs")
+const config = require('./config.json');
+
+const client = new Discord.Client()
+client.prefix = config.prefix;
+
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)){
+return message.channel.send(
+      "Olá " +
+        `<@${message.author.id}>` +
+        ", meu prefixo é `d-`. \n Precisa de ajuda use `d-ajuda`."
+    );
+  } 
+if(!message.content.startsWith(config.prefix)) return;
+let args = message.content.split(" ").slice(1);
+let command = message.content.split(" ")[0];
+command = command.slice(config.prefix.length);
+  try {
+      let commandFile = require(`./commands/${command}.js`);
+      delete require.cache[require.resolve(`./commands/${command}.js`)];
+      return commandFile.run(client, message, args);
+  } catch (err) {
+        console.error("Erro:" + err)
+  }
+})
+
+
+
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+        return console.log("[NeotiDev Sample-Bot] There aren't any events!");
+    }
+  jsfile.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+
+
+client.on("ready", () => {
+    console.log(`Bot foi iniciado com, ${client.users.size} usuários, ${client.guilds.size} servidores, ${client.channels.size} canais.`)
+
+    let statuses = [`Minha prefix é d-ajuda!`, `Linux ON 🐧`, `Segurança e Amor!`];
+   
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        
+        client.user.setPresence({
+            game: {
+                name: status,
+              type: "STREAMING"
+            },
+            status: 'online'
+        });
+    }, 10000)
+
+    //0 = Jogando
+    //1 = Transmitindo
+    //2 = Ouvindo
+    //3 = Assistindo
+  })
+
+client.login(config.token)
