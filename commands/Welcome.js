cf9722e69b1f0b8a16603e298799199b2fc45af5
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-03 14:50:11 -0300

    Update Welcome.js

--- commands/Welcome.js
+++ commands/Welcome.js
@@ -3,8 +3,9 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
 
 let embed = new Discord.RichEmbed()
-.setTitle(`:dbd: | Código:`) 
-.setDescription("```**Gatilho**: $onJoined[ ID DO CANAL DE BEM VINDO]\n\nComando:\n$nomention\n$description[Seja bem vindo **$username** ao servidor, agora temos **$membersCount** membros!] ```")
+.setTitle(`<:dbd:737869762486337548> | Código:`) 
+.addField("Gatilho", '```$onJoined[ ID DO CANAL DE BEM VINDO]```') 
+.addField("Comando", '```\n\nComando:\n$nomention\n$description[Seja bem vindo **$username** ao servidor, agora temos **$membersCount** membros!]```' )
 .setColor("#995BBD") 
 .setTimestamp() 
 .setFooter(`$message.author.tag`, message.author.avatarURL) 
