313545c6a8e85f180a02da1926eb3eaf34613485
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-03 14:39:04 -0300

    Create Clear.js

---
+++ commands/Clear.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+let embed = new Discord.RichEmbed()
+.setTitle(":dbd: | Código:") 
+.setDescription("```$nomention $deletecommand\n$clear[$message]\n$argsCheck[>1;❌ Você não colocou a quantidade de mensagens a ser deletada!]\n$color[#FF0000] \n$description[✅ <@$authorID> Apagou $message mensagens!] \n$onlyAdmin[❌ Você não tem permissão suficiente para utilizar este comando! ]```")
+.setColor("#995BBD") 
+.setTimestamp() 
+.setFooter(`$message.author.tag`, message.author.avatarURL) 
+
+message.channel.send(embed).then(msg => msg.delete(18000)) 
+} 
