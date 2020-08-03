ddb2204bb6ca90d56044f34aa195f681b9fc4e3d
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-03 14:53:47 -0300

    Update Clear.js

--- commands/Clear.js
+++ commands/Clear.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-
+message.delete() 
 let embed = new Discord.RichEmbed()
-.setTitle(`:dbd: | Código:`) 
+.setTitle(`<:dbd:737869762486337548> | Código:`) 
 .setDescription("```$nomention $deletecommand\n$clear[$message]\n$argsCheck[>1;❌ Você não colocou a quantidade de mensagens a ser deletada!]\n$color[#FF0000] \n$description[✅ <@$authorID> Apagou $message mensagens!] \n$onlyAdmin[❌ Você não tem permissão suficiente para utilizar este comando! ]```")
 .setColor("#995BBD") 
 .setTimestamp() 
