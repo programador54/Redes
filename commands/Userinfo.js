e7f1efcce430908ba4f38270e905f2c8d0e645a7
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-03 15:06:45 -0300

    Update Userinfo.js

--- commands/Userinfo.js
+++ commands/Userinfo.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
 
 let embed = new Discord.RichEmbed()
-.setTitle(":dbd: | Código:") 
+.setTitle("<:dbd:737869762486337548> | Código:") 
 .setDescription("```$nomention\n$userInfo[\n📚 **Nome Do Membro**:\n`{username}`\n💻 ** ID do Discord**:\n{ID}\n👥 **Discriminador**:\n#{discriminator}\n📆 Criado em:\n$creationDate[$mentioned[1]]\n]\n$argsCheck[1;Mencione alguém para saber sobre!]\n$color[#0000FF]\n$footer[Informações sobre o usuário!]```")
 .setColor("#995BBD") 
 .setTimestamp() 
