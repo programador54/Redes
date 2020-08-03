d4829b0c7596a703bf3a4c35e36adf7d022baaf9
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-03 15:37:47 -0300

    Create Welcome+Role.js

---
+++ commands/Welcome+Role.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+message.delete() 
+let embed = new Discord.RichEmbed()
+.setTitle("<:dbd:737869762486337548> | Código:") 
+.setDescription("```$nomention\n$title[👋 Seja Bem-vindo(a)!] \n$addReactions[👋]\n$color[#FF0000]\n$description[\nOlá <@$authorID>, obrigado por entrar em nosso servidor, agora temos **$membersCount** membros!] \n$thumbnail[$authorAvatar]\n]\n$addTimestamp\n$giveRole[$authorID;ID DO CARGO QUE VOCE QUER DA PARA QUEM ENTRAR]\n$image[https://media1.tenor.com/images/78c07c30bf3c7af7fc784da20cd6dbf8/tenor.gif]```")
+.setColor("#995BBD") 
+.setTimestamp() 
+.setFooter(`${message.author.tag}`, message.author.avatarURL) 
+
+message.channel.send(embed).then(msg => msg.delete(21000)) 
+} 
