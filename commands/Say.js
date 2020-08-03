e905d07d021de294057c6e2bf5c4e94e6e3906fe
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-03 14:56:03 -0300

    Update Say.js

--- commands/Say.js
+++ commands/Say.js
@@ -2,12 +2,13 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
+message.delete() 
 let embed = new Discord.RichEmbed()
 .setTitle("<:dbd:737869762486337548> | Código:") 
 .setDescription("```$nomention\n$deletecommand\n$message```")
 .setColor("#995BBD") 
 .setTimestamp() 
-.setFooter(`$message.author.tag`, message.author.avatarURL) 
+.setFooter(`${message.author.tag}`, message.author.avatarURL) 
 
 message.channel.send(embed).then(msg => msg.delete(18000)) 
 } 
