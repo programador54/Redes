6147c7ffeebbe4096d1bec8639f37da53a498b73
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-04 13:19:53 -0300

    Update and rename Códigos.js to comandos.js

--- commands/comandos.js
+++ commands/comandos.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 message.delete() 
 let embed = new Discord.RichEmbed()
 .setTitle("<a:Loanding:710604585399615498>  | Códigos DBD cadastrados no meu sistema:") 
-.setDescription("g-`Say` » Comando do bot falar no canal de Texto.\ng-`Clear` » Comando para limpar mensagens de um canal de texto.\ng-`Avatar` » Comando para ver a foto de um usuário.\ng-`Userinfo` » Comando para ver informações de um usuário.\ng-`Welcome` » Comando de boas vindas simples.\ng-`Welcome+Role` » Comando de boas vindas com imagem e que adiciona cargo ao membro que entrou.")
+.setDescription("g-`say` » Comando do bot falar no canal de Texto.\ng-`clear` » Comando para limpar mensagens de um canal de texto.\ng-`avatar` » Comando para ver a foto de um usuário.\ng-`userinfo` » Comando para ver informações de um usuário.\ng-`welcome` » Comando de boas vindas simples.\ng-`welcome2` » Comando de boas vindas com imagem e que adiciona cargo ao membro que entrou.")
 .setColor("#995BBD") 
 .setTimestamp() 
 .setFooter(`${message.author.tag}`, message.author.avatarURL) 
