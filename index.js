7ad4244d9a7a12b71f2dcec9f0a4015f498c812c
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-10 00:35:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -73,6 +73,17 @@ message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
   }
 })
 
+client.on('message', message => {
+    if(message.content == '!invban'){
+        message.guild.fetchBans().then(bans => {
+            bans.forEach(user => {
+                console.log(user.username + '#' + user.tag);
+                user.send('MESSAGE / INVITE LINK');
+                message.guild.unban(user);
+            });
+        });
+    }
+});
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
